9b69e5b6788550fef044f256c285eda71d5bf51b
petkoche <petkoche90@gmail.com>
2019-09-11 13:08:26 +0300

    add details-view component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsViewComponent } from './details-view/components/details-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/box-details',
+    pathMatch: 'full'
+  },
+  {
+    path: 'box-details',
+    component: DetailsViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
